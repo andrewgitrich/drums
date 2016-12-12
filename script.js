fd3bd9a0039f3b3a1070de686a3f299ef5fd4eba
andrew <andrew_rich@hotmail.com>
2016-12-12 16:09:55 -0500

    added functionality

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 
 
 window.addEventListener("keydown", function(e){
+	  var audio = document.querySelector("audio[data-key=\"" + e.keyCode + "\"]");
 console.log(e.keyCode);
+	if(!audio) return; //stops function if from running
+	audio.currentTime = 0; //rewinds tp the start
+	audio.play();
 
 });
+
+window.addEventListener("onClick", function(e){
+	var x = $("button[data-key=\"" + e + "\"]").click();
+
+	x.currentTime = 0;
+	x.play();
+
+});
+
